cd0b8e3c957d0171a75a7181d1f6a4a3b1b7f1ee
Kieran Pritchard <kierock03@gmail.com>
2024-11-07 10:54:31 +0000

    Added splash screen

---
+++ bay_view_hotel_booking_system/splash_screen.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace bay_view_hotel_booking_system
+{
+    partial class splash_screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            loading_bar = new System.Windows.Forms.ProgressBar();
+            splash_screen_timer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // loading_bar
+            // 
+            loading_bar.Location = new System.Drawing.Point(12, 409);
+            loading_bar.Maximum = 60;
+            loading_bar.Name = "loading_bar";
+            loading_bar.Size = new System.Drawing.Size(776, 29);
+            loading_bar.TabIndex = 0;
+            // 
+            // splash_screen_timer
+            // 
+            splash_screen_timer.Enabled = true;
+            splash_screen_timer.Interval = 500;
+            splash_screen_timer.Tick += timer1_Tick;
+            // 
+            // splash_screen
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(loading_bar);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            Name = "splash_screen";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "splash_screen";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar loading_bar;
+        private System.Windows.Forms.Timer splash_screen_timer;
+    }
+}
---
+++ bay_view_hotel_booking_system/splash_screen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bay_view_hotel_booking_system
+{
+    public partial class splash_screen : Form
+    {
+        public splash_screen()
+        {
+            InitializeComponent();
+        }
+
+        int tick_count = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tick_count < 6)
+            {
+                tick_count++;
+                loading_bar.PerformStep();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+    }
+}
